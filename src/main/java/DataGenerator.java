b218bac0ef57a32e3191b8b2a23b354f424496fe
ponyataeva <ponyataevaea@gmail.com>
2021-02-25 08:31:39 +0100

    adds generator of random data

--- src/main/java/DataGenerator.java
+++ src/main/java/DataGenerator.java
@@ -1,4 +1,14 @@
-package main.java;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
-public class DataGenerator {
+import java.util.Random;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class DataGenerator {
+
+    public static int[] generateData(int size) {
+        return new Random().ints(0, 100)
+                .limit(size)
+                .toArray();
+    }
 }
